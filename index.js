39ee96eebf336d9e5762fb924a175771f89d5150
Chaerin Park <53295724+pcr4769@users.noreply.github.com>
2020-01-03 00:24:29 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const title = document.querySelector("#title");
+
+const BASE_COLOR = "#00AFFF";
+const OTHER_COLOR = "#91D8FA";
+
+function handleClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handleClick);
+}
+
+init();
